e6c4ef9247041f7b438b57332d9ec07f86494d49
EgorMazkov <84621490+EgorMazkov@users.noreply.github.com>
2021-09-14 14:15:15 +0300

    Add files via upload

---
+++ minishell/Egor/include/minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ghumbert <ghumbert@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/14 12:28:12 by ghumbert          #+#    #+#             */
+/*   Updated: 2021/09/14 12:28:39 by ghumbert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include "../libs/libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+#endif
