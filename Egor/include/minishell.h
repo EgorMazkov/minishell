d2041c6c1f6ed1fc439b1d39d86c848d3d9efb83
Glynda Humberto <ghumbert@ge-k2.kzn.21-school.ru>
2021-09-16 16:27:32 +0300

    123\

--- Egor/include/minishell.h
+++ Egor/include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ghumbert <ghumbert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/14 12:28:12 by ghumbert          #+#    #+#             */
-/*   Updated: 2021/09/16 15:03:58 by ghumbert         ###   ########.fr       */
+/*   Updated: 2021/09/16 16:06:38 by ghumbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,18 @@
 # include <fcntl.h>
 # include <readline/history.h>
 # include <readline/readline.h>
+
+typedef struct s_ms
+{
+    char *input;
+    char **line;
+    int i;
+    int argc;
+    char **argv;
+    char **env;
+    char **way;
+} t_ms;
+
 
 /*readline*/
 void	rl_replace_line(const char *text, int clear_undo);
